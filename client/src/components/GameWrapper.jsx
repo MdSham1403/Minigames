12a401e9dc232752d505f3dde4a4dcecb2265b53
MdSham1403 <mdsham1403@gmail.com>
2026-05-27 23:15:44 +0530

    Removed

--- client/src/components/GameWrapper.jsx
+++ client/src/components/GameWrapper.jsx
@@ -1,54 +1,32 @@
-import ErrorBoundary        from './ErrorBoundary';
-import Snake                from '../games/Snake';
-import Memory               from '../games/Memory';
-import Game2048             from '../games/Game2048';
-import Trivia               from '../games/Trivia';
-import TicTacToe            from '../games/TicTacToe';
-import Sudoku               from '../games/Sudoku';
-import FlappyBird           from '../games/FlappyBird';
-import Breakout             from '../games/Breakout';
-import WordScramble         from '../games/WordScramble';
-import MathBlaster          from '../games/MathBlaster';
-import ColorMatch           from '../games/ColorMatch';
-import WhackAMole           from '../games/WhackAMole';
-import RockPaperScissors    from '../games/RockPaperScissors';
-import SimonSays            from '../games/SimonSays';
-import ReactionTime         from '../games/ReactionTime';
-import NumberPuzzle         from '../games/NumberPuzzle';
-import ConnectFour          from '../games/ConnectFour';
-import Chess                from '../games/Chess';
-import Ludo                 from '../games/Ludo';
-import UNO                  from '../games/UNO';
-import Minesweeper          from '../games/Minesweeper';
-import Tetris               from '../games/Tetris';
-import Hangman              from '../games/Hangman';
-import Battleship           from '../games/Battleship';
+import ErrorBoundary from './ErrorBoundary';
+import Snake from '../games/Snake';
+import Memory from '../games/Memory';
+import Game2048 from '../games/Game2048';
+import TicTacToe from '../games/TicTacToe';
+import Sudoku from '../games/Sudoku';
+import Breakout from '../games/Breakout';
+import MathBlaster from '../games/MathBlaster';
+import Chess from '../games/Chess';
+import Ludo from '../games/Ludo';
+import Minesweeper from '../games/Minesweeper';
+import Tetris from '../games/Tetris';
+import Hangman from '../games/Hangman';
+import Battleship from '../games/Battleship';
 
 const GAME_MAP = {
-  snake:        Snake,
-  memory:       Memory,
-  '2048':       Game2048,
-  trivia:       Trivia,
-  tictactoe:    TicTacToe,
-  sudoku:       Sudoku,
-  flappy:       FlappyBird,
-  breakout:     Breakout,
-  wordscramble: WordScramble,
-  mathblaster:  MathBlaster,
-  colormatch:   ColorMatch,
-  whackamole:   WhackAMole,
-  rps:          RockPaperScissors,
-  simon:        SimonSays,
-  reaction:     ReactionTime,
-  numberpuzzle: NumberPuzzle,
-  connect4:     ConnectFour,
-  chess:        Chess,
-  ludo:         Ludo,
-  uno:          UNO,
-  minesweeper:  Minesweeper,
-  tetris:       Tetris,
-  hangman:      Hangman,
-  battleship:   Battleship,
+  snake: Snake,
+  memory: Memory,
+  '2048': Game2048,
+  tictactoe: TicTacToe,
+  sudoku: Sudoku,
+  breakout: Breakout,
+  mathblaster: MathBlaster,
+  chess: Chess,
+  ludo: Ludo,
+  minesweeper: Minesweeper,
+  tetris: Tetris,
+  hangman: Hangman,
+  battleship: Battleship,
 };
 
 const GameWrapper = ({ gameId, onBack }) => {
